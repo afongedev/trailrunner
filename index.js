518d995e5a74bf441501ef53e1cd4580537ecd08
Andrew Fongemie <afonge.dev@gmail.com>
2021-05-29 07:20:04 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+    });
+
+      
+// app.get("/runmappop", function (req, res) {
+//     res.render("runMapPopUp.ejs");
+//     });
+         
+      
+// Set up listener
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+
